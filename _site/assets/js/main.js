ba8717bf475dd20b7ede34997dfa45afcaf89652
Joe Buhlig <joe@joebuhlig.com>
2015-10-15 06:36:09 -0500

    Added shadow to menu on scrolling

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -3,5 +3,17 @@ $(document).ready(function(){
 	$('.menu-icon').click(function(event){
 		event.preventDefault();
 		$('.trigger').toggleClass('active');
-	})
+	});
+
+	var topOfContent = $(".page-content").offset().top;
+
+	$(window).scroll(function() {
+	    var topOfWindow = $(window).scrollTop() + $('.site-nav-outer').height();
+	    if (topOfContent < topOfWindow) {
+	        $(".site-nav-outer").addClass("scrolled");          
+	    }
+	    else {
+	        $(".site-nav-outer").removeClass("scrolled");
+	    }
+});
 });
