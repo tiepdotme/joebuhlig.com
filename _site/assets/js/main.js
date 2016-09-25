73e097e2694b9136589a0955cc987b03333d7cd0
Joe Buhlig <joe@joebuhlig.com>
2016-09-25 08:03:39 -0500

    Removed old search functionality

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,12 +1,6 @@
 if (getCookie("theme") == "light"){
 	$('html').removeClass("dark");
 }
-var searchJSONFile = '/assets/js/clean-search.json',
-		$searchBar = null,
-		$searchBarInput = null,
-		$searchResults = null,
-		currentSearchTerm = '',
-		jsonLoaded = false;
 
 $(document).ready(function(){
 	var topOfContent = $(".page-content").offset().top;
@@ -42,88 +36,6 @@ $(document).ready(function(){
 			document.cookie = "theme=light";
 		}
 	})
-
-
-	$searchBar = $('#search-bar');
-	$searchBarInput = $('#search-bar-input');
-	$searchResults = $('#search-results');
-
-	window.toggleSearch = function toggleSearch() {
-		if (!jsonLoaded){
-			$.getJSON(searchJSONFile, function(data) {
-			    searchData = data["clean_search"];
-			    jsonLoaded = true;
-			});
-		}
-
-		if ($searchBar.hasClass('active-search')) {
-			$searchBar.removeClass('active-search');
-			$searchResults.html("");
-		} else {
-			$searchBar.addClass('active-search');
-			$searchResults.html("");
-			$searchBarInput.val("");
-			setTimeout(function(){
-				$searchBarInput.focus();
-			}, 200);
-		}
-	};
-
-	// Toggle on ESC key
-	$(window).on('keyup', function onKeyPress(e) {
-		if (e.which === 27) {
-			$searchBar.removeClass('active-search');
-		}
-	});
-	// Open on '/' key
-	$(window).on('keypress', function onKeyPress(e) {
-		if (e.which === 47 && !$searchBar.hasClass('active-search')) {
-			toggleSearch();
-		}
-	});
-
-	$searchBarInput.on('input', function() {
-		searchJSON();
-	});	
-
-	$('#search-icon').click(function(e){
-		e.preventDefault();
-		toggleSearch();
-	});
-	$('#search-close').click(function(e){
-		e.preventDefault();
-		toggleSearch();
-	});
-	$searchResults.click(function(){
-		toggleSearch();
-	});
-	function searchJSON() {
-		$searchResults.html("");
-
-		currentSearchTerm = $searchBarInput.val();
-
-		if (!currentSearchTerm || currentSearchTerm.length < 2) {
-			$searchResults.html("");
-			return;
-		}
-
-		var newResults = "";
-		for (var post in searchData) {
-			if ( searchData[post].title.match(new RegExp(currentSearchTerm, "i")) !== null || searchData[post].content.match(new RegExp(currentSearchTerm, "i")) !== null) {
-				newResults = newResults + '<div class="search-result-item">';
-				newResults = newResults + '<div class="search-result-image-wrapper">';
-				newResults = newResults + '<a href="' + searchData[post].permalink + '">';
-				newResults = newResults + '<img class="search-result-image" src="' + searchData[post].image + '" />';
-				newResults = newResults + '</a>';
-				newResults = newResults + '</div>';
-				newResults = newResults + '<div class="search-result-excerpt">';
-				newResults = newResults + searchData[post].excerpt;
-				newResults = newResults + '</div>';
-				newResults = newResults + '</div>';
-			}
-		}
-		$searchResults.html(newResults);
-	}
 });
 
 function displayImages(loc){
