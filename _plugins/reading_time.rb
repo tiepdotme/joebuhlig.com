daf03b374bd5b8aa7e57c21b65a8883608e34e23
Joe Buhlig <joe@joebuhlig.com>
2019-06-24 16:03:44 -0500

    Create tiling

--- _plugins/reading_time.rb
+++ _plugins/reading_time.rb
@@ -5,13 +5,20 @@
 # Usage: Read this in about {{ page.content | reading_time }}
 
 module ReadingTimeFilter
-	def reading_time( input )
+  def reading_time( content, post )
 		words_per_minute = 200
+    if post.categories == 'blog'
+      words = content.split.size;
+      minutes = ( words / words_per_minute ).floor
+      minutes_label = minutes === 1 ? " min" : " mins"
+      minutes > 0 ? "#{minutes} #{minutes_label}" : "> 1 min"
+    else if post.categories == 'bookworm'
+      
+    else if post.categories == 'whims'
 
-		words = input.split.size;
-		minutes = ( words / words_per_minute ).floor
-		minutes_label = minutes === 1 ? " min" : " mins"
-		minutes > 0 ? "~#{minutes} #{minutes_label}" : "> 1 min"
+    else
+
+    end
 	end
 end
 
